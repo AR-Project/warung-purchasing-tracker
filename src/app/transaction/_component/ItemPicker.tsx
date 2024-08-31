b868eb782ee2855a22c9544fe3fabab240dc0951
AR-Project <alvenresta@gmail.com>
2024-08-31 19:21:04 +0700

    initial implementation

---
+++ src/app/transaction/_component/ItemPicker.tsx
@@ -0,0 +1,208 @@
+"use client";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+} from "@headlessui/react";
+import { toast } from "react-toastify";
+import { LuLoader2 } from "react-icons/lu";
+import { useRouter } from "next/navigation";
+import { HiSearch } from "react-icons/hi";
+
+import { useDelayQuery } from "@/presentation/hooks/useDelayQuery";
+import { searchItems } from "@/lib/api";
+
+import EditItemHiddenForm from "@/app/create/_component/EditItemHiddenForm";
+import { ResetItemInputButton } from "@/app/create/_component/ResetItemInputButton";
+import Tooltip from "@/presentation/component/Tooltip";
+
+const INITIAL: Vendor = { id: "", name: "" };
+
+type Props = {
+  activeName: string | undefined;
+};
+
+export default function ItemPicker({ activeName }: Props) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+  const [itemsSelection, setItemsSelection] = useState<Item[]>([]);
+
+  const [query, setQuery] = useState(activeName ? activeName : "");
+  const [isQueryChanged, setIsQueryChanged] = useState<boolean>(false);
+
+  const [delayedQuery] = useDelayQuery(query, 500);
+  const [isDelayedQueryChanged, setIsDelayedQueryChanged] =
+    useState<boolean>(false);
+
+  const [selectedItem, setSelectedItem] = useState<Item>({ id: "", name: "" });
+
+  const comboBoxOnChangeHandler = (value: NoInfer<Item> | null) => {
+    value && setSelectedItem(value);
+  };
+
+  const onCloseComboBox = () => {
+    setQuery("");
+  };
+
+  const comboBoxDisplayValue = (item: Item) => {
+    return item.name;
+  };
+
+  const itemFieldRef = useRef<HTMLInputElement>(null);
+
+  const searchItemsAction = async (delayedQuery: string) => {
+    if (delayedQuery.length < 3) return;
+    setLoading(true);
+    try {
+      const response = await searchItems(delayedQuery);
+      if (response.status === 200) {
+        const data = (await response.json()) as unknown as Item[];
+        setItemsSelection(data);
+      } else {
+        setItemsSelection([]);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const enableSubmitButton = () => {
+    const isItemSelected = selectedItem.id !== "";
+
+    if (isItemSelected) return true;
+    return false;
+  };
+
+  const displayEditButton = () => {
+    const isItemSelected = selectedItem.id !== "";
+    const isQueryEmpty = query === "";
+
+    if (isItemSelected && isQueryEmpty) return true;
+    return false;
+  };
+
+  useEffect(() => {
+    setIsQueryChanged(true);
+  }, [query]);
+
+  if (isQueryChanged) {
+    setItemsSelection([]);
+    setError(false);
+    setIsQueryChanged(false);
+  }
+
+  useEffect(() => {
+    setIsDelayedQueryChanged(true);
+  }, [delayedQuery]);
+
+  if (isDelayedQueryChanged) {
+    searchItemsAction(delayedQuery);
+    setIsDelayedQueryChanged(false);
+  }
+
+  const setNameQuery = (name: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("q", name);
+
+    return params.toString();
+  };
+
+  const removeNameQuery = () => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.delete("q");
+    return params.toString();
+  };
+
+  const searchItem = () => {
+    router.push(pathname + "?" + setNameQuery(selectedItem.name));
+  };
+
+  const resetComboForm = () => {
+    router.push(pathname + "?" + removeNameQuery());
+    setSelectedItem(INITIAL);
+    setQuery("");
+  };
+
+  return (
+    <div className="flex flex-col gap-2">
+      <Combobox
+        value={selectedItem}
+        onChange={comboBoxOnChangeHandler}
+        onClose={onCloseComboBox}
+      >
+        <div className="flex flex-row items-center">
+          <ComboboxInput
+            aria-label="Assignee"
+            ref={itemFieldRef}
+            displayValue={comboBoxDisplayValue}
+            onChange={(event) => setQuery(event.target.value)}
+            className={`${
+              activeName ? "bg-green-700" : "bg-gray-800"
+            } px-2 h-10 w-full border ${
+              error ? "border-red-500" : "border-gray-600"
+            }`}
+            placeholder="Ketik nama item..."
+            disabled={selectedItem.name !== ""}
+          />
+          {/* {displayEditButton() && (
+            <EditItemHiddenForm
+              selectedItem={selectedItem}
+              setSelectedItem={setSelectedItem}
+            />
+          )} */}
+
+          {selectedItem.id !== "" && (
+            <div className="group relative">
+              <ResetItemInputButton resetComboForm={resetComboForm} />
+              <Tooltip>Hapus Filter</Tooltip>
+            </div>
+          )}
+          {loading && <LuLoader2 className="ml-3 animate-spin text-3xl" />}
+          {!activeName && (
+            <button
+              onClick={searchItem}
+              className="h-10 aspect-square flex flex-row justify-center items-center bg-blue-900 hover:bg-blue-800 border-t border-r border-b border-gray-600 text-white p-1 rounded-sm w-fit ml-auto disabled:bg-blue-600/30 disabled:text-white/20 disabled:cursor-not-allowed"
+              disabled={!enableSubmitButton()}
+            >
+              <HiSearch className="text-2xl" />
+            </button>
+          )}
+
+          <ComboboxOptions
+            anchor="bottom"
+            className="border bg-gray-800 empty:invisible"
+          >
+            {itemsSelection.map((item) => (
+              <ComboboxOption
+                key={item.id}
+                value={item}
+                className="data-[focus]:bg-gray-500 p-3"
+              >
+                {item.name}
+              </ComboboxOption>
+            ))}
+          </ComboboxOptions>
+        </div>
+      </Combobox>
+
+      <div className="flex flex-row-reverse">
+        {error && (
+          <p className="text-red-500 text-xs uppercase italic">
+            Barang sudah terdaftar. Ganti atau ubah barang
+          </p>
+        )}
+      </div>
+    </div>
+  );
+}
