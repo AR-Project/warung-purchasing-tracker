fdef8e2e933bb71e6c4ca3b404c9420794f48377
AR-Project <alvenresta@gmail.com>
2024-09-11 17:10:13 +0700

    chore:clean up remove comment and unnesecary import

--- src/app/transaction/page.tsx
+++ src/app/transaction/page.tsx
@@ -1,20 +1,3 @@
-import Link from "next/link";
-import { transactionLoader } from "./purchase/transaction.loaders.action";
-import { SinglePurchaseCard } from "./_component/SinglePurchaseCard";
-
 export default async function Page() {
-  const tx = await transactionLoader();
-
-  /** #TODO: Implement display based on these
-   * - Based on month
-   * - FUTURE:Based on calendar
-   *    - Highlight dates that purchase happen
-   *    - Pop up transaction details from that date
-   */
-
-  function prettyStringify(object: any) {
-    return JSON.stringify(object, null, "\t");
-  }
-
   return <h1>Pick One...</h1>;
 }
