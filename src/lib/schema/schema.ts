f47313e8fb2a2452396c8c1f42ddcf58f6ed7130
AR-Project <alvenresta@gmail.com>
2024-09-20 18:02:16 +0700

    adding index on some column
    name on item
    itemid on purchase item

--- src/lib/schema/schema.ts
+++ src/lib/schema/schema.ts
@@ -1,6 +1,7 @@
 import { sql } from "drizzle-orm";
 import {
   boolean,
+  index,
   integer,
   pgTable,
   serial,
@@ -18,30 +19,46 @@ export const images = pgTable("images", {
     .notNull(),
 });
 
-export const items = pgTable("items", {
-  id: text("id").primaryKey().unique().notNull(),
-  name: text("name").unique().notNull(),
-  imageId: text("image_id").references(() => images.id, {
-    onDelete: "set null",
-  }),
-  createdAt: timestamp("created_at", { withTimezone: true })
-    .defaultNow()
-    .notNull(),
-  modifiedAt: timestamp("last_modified_at", { withTimezone: true })
-    .defaultNow()
-    .notNull(),
-});
+export const items = pgTable(
+  "items",
+  {
+    id: text("id").primaryKey().unique().notNull(),
+    name: text("name").unique().notNull(),
+    imageId: text("image_id").references(() => images.id, {
+      onDelete: "set null",
+    }),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .defaultNow()
+      .notNull(),
+    modifiedAt: timestamp("last_modified_at", { withTimezone: true })
+      .defaultNow()
+      .notNull(),
+  },
+  (table) => {
+    return {
+      itemsNameIdx: index("item_name_idx").on(table.name),
+    };
+  }
+);
 
-export const vendors = pgTable("vendors", {
-  id: text("id").primaryKey().unique().notNull(),
-  name: text("name").unique().notNull(),
-  createdAt: timestamp("created_at", { withTimezone: true })
-    .defaultNow()
-    .notNull(),
-  modifiedAt: timestamp("last_modified_at", { withTimezone: true })
-    .defaultNow()
-    .notNull(),
-});
+export const vendors = pgTable(
+  "vendors",
+  {
+    id: text("id").primaryKey().unique().notNull(),
+    name: text("name").unique().notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .defaultNow()
+      .notNull(),
+    modifiedAt: timestamp("last_modified_at", { withTimezone: true })
+      .defaultNow()
+      .notNull(),
+  },
+  (table) => {
+    return {
+      vendorNameIdx: index("vendor_name_idx").on(table.name),
+    };
+  }
+);
 
 export const purchases = pgTable("purchases", {
   id: text("id").primaryKey().unique().notNull(),
@@ -65,16 +82,24 @@ export const purchases = pgTable("purchases", {
   totalPrice: integer("total_price").notNull(),
 });
 
-export const purchasedItems = pgTable("purchased_items", {
-  id: text("id").primaryKey().unique().notNull(),
-  purchaseId: text("purchase_id")
-    .references(() => purchases.id)
-    .notNull(),
-  itemId: text("item_id")
-    .references(() => items.id)
-    .notNull(),
-  quantityInHundreds: integer("quantity_in_hundreds").notNull(),
-  pricePerUnit: integer("price_per_unit").notNull(),
-  totalPrice: integer("total_price").notNull(),
-  isDeleted: boolean("is_deleted").default(false).notNull(),
-});
+export const purchasedItems = pgTable(
+  "purchased_items",
+  {
+    id: text("id").primaryKey().unique().notNull(),
+    purchaseId: text("purchase_id")
+      .references(() => purchases.id, { onDelete: "cascade" })
+      .notNull(),
+    itemId: text("item_id")
+      .references(() => items.id)
+      .notNull(),
+    quantityInHundreds: integer("quantity_in_hundreds").notNull(),
+    pricePerUnit: integer("price_per_unit").notNull(),
+    totalPrice: integer("total_price").notNull(),
+    isDeleted: boolean("is_deleted").default(false).notNull(),
+  },
+  (table) => {
+    return {
+      purchaseditemIdIdx: index("purchased_item_id_idx").on(table.itemId),
+    };
+  }
+);
