c121740422c087e9f39ec427293a5b2b986d4c65
AR-Project <alvenresta@gmail.com>
2024-10-02 16:06:04 +0700

    Change card element from div to link

--- src/app/transaction/_component/SinglePurchaseCard.tsx
+++ src/app/transaction/_component/SinglePurchaseCard.tsx
@@ -1,11 +1,8 @@
-import {
-  shortDateWithDay,
-  formatNumberToIDR,
-  shortDate,
-} from "@/lib/utils/formatter";
-import { DisplaySingleItem } from "@/presentation/component/DisplaySingleItem";
 import Link from "next/link";
 import { MdReceiptLong } from "react-icons/md";
+
+import { shortDateWithDay, formatNumberToIDR } from "@/lib/utils/formatter";
+import { DisplaySingleItem } from "@/presentation/component/DisplaySingleItem";
 
 type Props = {
   singlePurchase: DisplaySinglePurchase;
@@ -13,36 +10,39 @@ type Props = {
 
 export function SinglePurchaseCard({ singlePurchase }: Props) {
   return (
-    <div className="card bg-gray-900 border border-gray-700 mb-4">
-      <hgroup className="bg-blue-900 p-2 flex flex-row justify-between">
-        <div className="flex flex-col items-baseline uppercase ">
-          <h2 className="font-bold">{singlePurchase.vendorName}</h2>
-          <time className="text-xs">
-            {shortDateWithDay(singlePurchase.purchasesAt)}
-          </time>
+    <div className="flex flex-col">
+      <Link
+        href={`/transaction/details/${singlePurchase.id}`}
+        className="card bg-gray-900 border border-gray-700"
+      >
+        <hgroup className="bg-blue-900 p-2 flex flex-row justify-between">
+          <div className="flex flex-col items-baseline uppercase ">
+            <time className="font-bold">
+              {shortDateWithDay(singlePurchase.purchasesAt)}
+            </time>
+            <h2 className="text-xs">{singlePurchase.vendorName}</h2>
+          </div>
+          <div className="font-black font-mono">
+            {formatNumberToIDR(singlePurchase.totalPrice, "short")}
+          </div>
+        </hgroup>
+        <div className="px-2 py-1 flex flex-col gap-2">
+          {singlePurchase.items.map((item) => (
+            <DisplaySingleItem key={item.id} item={item} />
+          ))}
         </div>
-        <div className="font-black font-mono">
-          {formatNumberToIDR(singlePurchase.totalPrice)}
-        </div>
-      </hgroup>
-      <div className="px-2 py-1 flex flex-col gap-2">
-        {singlePurchase.items.map((item) => (
-          <DisplaySingleItem key={item.id} item={item} />
-        ))}
-      </div>
-      <div className=" flex flex-row justify-center">
-        {singlePurchase.imageId && (
-          // TODO: Pop Up Image
-          <Link
-            className=" flex flex-row items-center gap-2 text-gray-500"
-            href={`/api/image/${singlePurchase.imageId}.jpg`}
-            target="_blank"
-          >
-            <MdReceiptLong />
-            <span className="text-sm">Show Receipt</span>
-          </Link>
-        )}
-      </div>
+        <div className=" flex flex-row justify-center"></div>
+      </Link>
+      {singlePurchase.imageId && (
+        <Link
+          className="h-8 flex flex-row items-center gap-2 text-gray-500 w-full justify-center border-gray-700 border-r border-b border-l"
+          href={`/api/image/${singlePurchase.imageId}.jpg`}
+          target="_blank"
+        >
+          <MdReceiptLong />
+          <span className="text-sm">Show Receipt</span>
+        </Link>
+      )}
     </div>
   );
 }
