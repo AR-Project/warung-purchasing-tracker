120c58a0a1e7759d53a004e0236e8971a1e15f24
AR-Project <alvenresta@gmail.com>
2024-10-11 15:39:58 +0700

    style/refactor div and change text formatting

--- src/presentation/component/DisplaySingleItem.tsx
+++ src/presentation/component/DisplaySingleItem.tsx
@@ -1,23 +1,20 @@
 import { formatNumberToIDR } from "@/lib/utils/formatter";
 import { PrettyQuantity } from "@/presentation/component/PrettyQuantity";
-import { NumericFormat, PatternFormat } from "react-number-format";
 
 export function DisplaySingleItem({ item }: { item: DisplaySingleItem }) {
   return (
-    <div className="font-mono w-full max-w-[500px] mx-auto flex flex-row gap-3 ">
-      <div className="text-sm uppercase w-full ">
-        <div className="font-bold">{item.name}</div>
-        <div className="text-sm flex flex-row justify-between gap-3">
-          <div className="flex flex-row">
-            <PrettyQuantity number={item.quantityInHundreds} />
-            <p>⨯ {formatNumberToIDR(item.pricePerUnit, "short")}</p>
-          </div>
-          <p>
-            {formatNumberToIDR(
-              item.pricePerUnit * (item.quantityInHundreds / 100),
-              "short"
-            )}
-          </p>
+    <div className="font-mono max-w-[350px] mx-auto flex flex-col leading-tight uppercase w-full ">
+      <div className="font-bold">{item.name}</div>
+      <div className=" flex flex-row justify-between gap-3">
+        <div className="opacity-60 flex flex-row">
+          <PrettyQuantity number={item.quantityInHundreds} />
+          <p>⨯ {formatNumberToIDR(item.pricePerUnit, "short")}</p>
+        </div>
+        <div className="font-bold">
+          {formatNumberToIDR(
+            item.pricePerUnit * (item.quantityInHundreds / 100),
+            "short"
+          )}
         </div>
       </div>
     </div>
