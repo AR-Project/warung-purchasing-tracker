c086163b9cc8b3a33c55239a9dff997aab134e61
AR-Project <alvenresta@gmail.com>
2024-10-11 22:48:43 +0700

    refactor/ on items editor - move a component one level up

--- src/app/transaction/details/[purchaseId]/page.tsx
+++ src/app/transaction/details/[purchaseId]/page.tsx
@@ -1,4 +1,8 @@
+import Link from "next/link";
+
 import { PurchaseEditor } from "./_component/PurchaseEditor";
+import PurchaseItemUpdater from "./_component/PurchaseItemUpdater";
+import PurchaseItemDisplayer from "./_component/PurchaseItemDisplayer";
 import { singlePurchaseLoader } from "./_loader/singlePurchase.loader";
 import { BackButton } from "./_presentation/BackButton";
 
@@ -10,16 +14,30 @@ export default async function Page({ params }: Props) {
   const details = await singlePurchaseLoader(params.purchaseId);
   if (!details) return <div>Transaction Not Found</div>;
 
+  const { items: purchaseItems, totalPrice, id: purchaseId } = details;
+
   return (
-    <>
+    <div>
       <div className="flex flex-row gap-3 items-center mb-4">
         <BackButton />
         <div>Purchase Details</div>
       </div>
       <PurchaseEditor purchase={details} />
-      <pre className="text-xs text-gray-600">
-        {JSON.stringify(details, null, 2)}
-      </pre>
-    </>
+
+      <PurchaseItemDisplayer
+        purchaseId={purchaseId}
+        purchaseItems={purchaseItems}
+        totalPrice={totalPrice}
+      />
+      <div className="flex flex-row gap-2">
+        <PurchaseItemUpdater purchaseId={purchaseId} />
+        <Link
+          className="bg-blue-950 border border-gray-500 group-hover:bg-blue-800  h-8 px-2 text-gray-100 flex flex-row items-center gap-3 justify-center cursor-not-allowed"
+          href="#"
+        >
+          Change Item Order
+        </Link>
+      </div>
+    </div>
   );
 }
