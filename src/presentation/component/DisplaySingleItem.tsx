c55fe77a8dd46aa5ec476c584e80d6ba3d737681
AR-Project <alvenresta@gmail.com>
2024-10-15 14:41:26 +0700

    feat/change item name to link to single item detail

--- src/presentation/component/DisplaySingleItem.tsx
+++ src/presentation/component/DisplaySingleItem.tsx
@@ -1,10 +1,16 @@
 import { formatNumberToIDR } from "@/lib/utils/formatter";
 import { PrettyQuantity } from "@/presentation/component/PrettyQuantity";
+import Link from "next/link";
 
 export function DisplaySingleItem({ item }: { item: DisplaySingleItem }) {
   return (
     <div className="font-mono  flex flex-col leading-tight uppercase w-full ">
-      <div className="font-bold">{item.name}</div>
+      <Link
+        href={`/transaction/item/detail/${item.itemId}`}
+        className="font-bold"
+      >
+        {item.name}
+      </Link>
       <div className=" flex flex-row justify-between gap-3">
         <div className="opacity-60 flex flex-row">
           <PrettyQuantity number={item.quantityInHundreds} />
