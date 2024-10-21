09fd848e4a3477a7b1efd4550ae33ad26e8c93d6
AR-Project <alvenresta@gmail.com>
2024-10-21 20:17:37 +0700

    chore/remove fragment

--- src/app/transaction/item/page.tsx
+++ src/app/transaction/item/page.tsx
@@ -5,19 +5,17 @@ export default async function Page() {
   const listOfItems = await listOfItemsLoader();
 
   return (
-    <>
-      <div className="flex flex-col gap-1 max-w-md mx-auto">
-        {listOfItems.map((item) => (
-          <Link
-            className="h-10 px-3 bg-gray-700 hover:bg-gray-600 border border-gray-400 flex flex-row justify-start items-center"
-            key={item.id}
-            href={`./item/detail/${item.id}`}
-          >
-            {item.name}
-          </Link>
-        ))}
-      </div>
-    </>
+    <div className="flex flex-col gap-1 max-w-md mx-auto">
+      {listOfItems.map((item) => (
+        <Link
+          className="h-10 px-3 bg-gray-700 hover:bg-gray-600 border border-gray-400 flex flex-row justify-start items-center"
+          key={item.id}
+          href={`./item/detail/${item.id}`}
+        >
+          {item.name}
+        </Link>
+      ))}
+    </div>
   );
 }
 
