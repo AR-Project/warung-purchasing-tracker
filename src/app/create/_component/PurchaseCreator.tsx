706ff58290392f64a43fc9c4eb9b94af70a26e58
AR-Project <alvenresta@gmail.com>
2024-10-26 19:33:54 +0700

    feat/refactor logic - implement useCartManager hooks

--- src/app/create/_component/PurchaseCreator.tsx
+++ src/app/create/_component/PurchaseCreator.tsx
@@ -6,20 +6,33 @@ import { formatNumberToIDR } from "@/lib/utils/formatter";
 import { ItemOnCartCard } from "../_presentation/ItemOnCartCard";
 import MakePurchaseHiddenForm from "./MakePurchaseHiddenForm";
 import DatePicker from "./DatePicker";
+import useCartManager from "./_hooks/useCartManager";
 
 const ImageSelector = dynamic(() => import("./ImageSelector"));
 const ComboItemForm = dynamic(() => import("./ComboItemForm"));
 const ComboVendorForm = dynamic(() => import("./ComboVendorForm"));
 
+type DataTemplate = { id: string; name: string };
+
 type Props = {
-  initialVendors: { id: string; name: string }[];
+  initialVendors: DataTemplate[];
+  initialItems: DataTemplate[];
 };
 
-export default function PurchaseCreator({ initialVendors }: Props) {
-  const [itemsOnCart, setItemsOnCart] = useState<CreatePurchaseItemWithName[]>(
-    []
-  );
-  const [selectedItemOnCart, setSelectedItemOnCart] = useState<string>("");
+export default function PurchaseCreator({
+  initialVendors,
+  initialItems,
+}: Props) {
+  const {
+    itemsCart,
+    activeItemOnCart,
+    cartTotalPrice,
+    appendItemToCart,
+    updateItemOnCart,
+    deleteItemOnCart,
+    resetCart,
+    itemOnCartClickHandler,
+  } = useCartManager();
 
   const [selectedVendor, setSelectedVendor] = useState<Vendor | null>(null);
   const [txDate, setTxDate] = useState<string>("");
@@ -29,49 +42,11 @@ export default function PurchaseCreator({ initialVendors }: Props) {
     setSelectedVendor(data);
   }
 
-  // Manipulate Item List
-  const appendItemOnCart = (item: CreatePurchaseItemWithName) => {
-    const isAlreadyAdded =
-      itemsOnCart.filter((addedItem) => addedItem.itemId == item.itemId)
-        .length > 0;
-
-    if (isAlreadyAdded) return "Tambahkan item lain";
-    setItemsOnCart((prevItems) => [...prevItems, item]);
-  };
-
-  const updateItemOnCart = (
-    updatedItem: CreatePurchaseItemWithName,
-    index: number
-  ) => {
-    setItemsOnCart((prevItemsList) => {
-      const newList = [...prevItemsList];
-      newList[index] = updatedItem;
-      return newList;
-    });
-  };
-
-  const deleteItemOnCart = (itemIndex: number) => {
-    setItemsOnCart((prevItems) =>
-      [...prevItems].filter((_, index) => index != itemIndex)
-    );
-  };
-
-  const itemOnCartClickHandler = (itemId: string) => {
-    setSelectedItemOnCart((prev) => {
-      return prev == itemId ? "" : itemId;
-    });
-  };
-
-  const totalPurchase = itemsOnCart.reduce(
-    (accumulator, item) => accumulator + item.totalPrice,
-    0
-  );
-
   function resetAddTxForm() {
     setTxDate("");
     setSelectedVendor(null);
     setResizedImage(null);
-    setItemsOnCart([]);
+    resetCart();
   }
 
   return (
@@ -93,18 +68,18 @@ export default function PurchaseCreator({ initialVendors }: Props) {
         />
       </Suspense>
 
-      {itemsOnCart.length === 0 && (
+      {itemsCart.length === 0 && (
         <span className="w-full h-full italic text-center text-sm text-gray-600 border border-gray-600/50 p-3">
           Transaksi kosong...
         </span>
       )}
-      {itemsOnCart.length > 0 && (
+      {itemsCart.length > 0 && (
         <div className="max-h-64 overflow-y-scroll flex flex-col font-mono">
-          {itemsOnCart.map((item, index) => (
+          {itemsCart.map((item, index) => (
             <ItemOnCartCard
               key={item.itemId}
               onClick={itemOnCartClickHandler}
-              isActive={selectedItemOnCart == item.itemId}
+              isActive={activeItemOnCart == item.itemId}
               item={item}
               index={index}
               deleteItem={deleteItemOnCart}
@@ -115,17 +90,17 @@ export default function PurchaseCreator({ initialVendors }: Props) {
       )}
 
       <h4 className="text-xl font-black text-center border border-gray-700 p-2">
-        {formatNumberToIDR(totalPurchase)}
+        {formatNumberToIDR(cartTotalPrice)}
       </h4>
       <Suspense fallback={<span>loading...</span>}>
-        <ComboItemForm appendItemOnCart={appendItemOnCart} />
+        <ComboItemForm appendItemOnCart={appendItemToCart} />
       </Suspense>
 
       <MakePurchaseHiddenForm
         purchasedAt={txDate}
         vendorId={selectedVendor ? selectedVendor.id : null}
-        listOfPurchaseItem={itemsOnCart}
-        totalPurchase={totalPurchase}
+        listOfPurchaseItem={itemsCart}
+        totalPurchase={cartTotalPrice}
         image={resizedImage}
         clearFrom={resetAddTxForm}
       />
--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -2,16 +2,21 @@ import { auth } from "@/auth";
 import LoginRequiredWarning from "../_component/auth/LoginRequiredWarning";
 import PurchaseCreator from "./_component/PurchaseCreator";
 import getUserVendors from "../_loader/getUserVendors.loader";
+import getUserItems from "../_loader/getUserItems.loader";
 
 export default async function Create() {
   const session = await auth();
   if (!session) return <LoginRequiredWarning />;
 
   const vendorsInitialData = await getUserVendors(session.user.parentId);
+  const itemsInitialData = await getUserItems(session.user.parentId);
 
   return (
     <>
-      <PurchaseCreator initialVendors={vendorsInitialData} />
+      <PurchaseCreator
+        initialVendors={vendorsInitialData}
+        initialItems={itemsInitialData}
+      />
     </>
   );
 }
