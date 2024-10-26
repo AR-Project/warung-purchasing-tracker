72c0ef01442b97f3b4763bb09ff6a7dec0485140
AR-Project <alvenresta@gmail.com>
2024-10-26 19:32:49 +0700

    feat/init getUserItems loader

---
+++ src/app/_loader/getUserItems.loader.ts
@@ -0,0 +1,10 @@
+import db from "@/infrastructure/database/db";
+import { items } from "@/lib/schema/schema";
+import { eq } from "drizzle-orm";
+
+export default async function getUserItems(userId: string) {
+  return await db
+    .select({ id: items.id, name: items.name })
+    .from(items)
+    .where(eq(items.ownerId, userId));
+}
