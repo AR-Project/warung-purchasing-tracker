3c616bbc5614f35070c56bd479ac4c86fb2450b1
AR-Project <alvenresta@gmail.com>
2024-10-29 18:16:07 +0700

    feat/only allow spesific role to make transaction

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -3,10 +3,16 @@ import LoginRequiredWarning from "../_component/auth/LoginRequiredWarning";
 import PurchaseCreator from "./_component/PurchaseCreator";
 import getUserVendors from "../_loader/getUserVendors.loader";
 import getUserItems from "../_loader/getUserItems.loader";
+import getUserRole from "../_loader/getUserRole.loader";
+import NotAllowedWarning from "../_component/auth/NotAllowedWarning";
 
 export default async function Create() {
   const session = await auth();
   if (!session) return <LoginRequiredWarning />;
+
+  const allowedRole: AvailableUserRole[] = ["admin", "manager", "staff"];
+  const role = await getUserRole(session.user.userId);
+  if (!allowedRole.includes(role)) return <NotAllowedWarning />;
 
   const vendorsInitialData = await getUserVendors(session.user.parentId);
   const itemsInitialData = await getUserItems(session.user.parentId);
