9d1460676a3ce34c4c436ab28bb7bea22c962c6a
AR-Project <alvenresta@gmail.com>
2024-11-01 16:39:42 +0700

    chore/move database action to own files (repository)

--- src/app/_globalAction/registerUser.action.ts
+++ src/app/_globalAction/registerUser.action.ts
@@ -1,76 +1,51 @@
 "use server";
-import { eq } from "drizzle-orm";
 import { z } from "zod";
 import { hash } from "@node-rs/argon2";
 
-import db from "@/infrastructure/database/db";
-import { user } from "@/lib/schema/user";
+import { NewUserDbPayload } from "@/lib/schema/user";
 import { generateId } from "@/lib/utils/generator";
+import { saveNewUser } from "@/infrastructure/repository/userRepository";
 
 export async function registerUserAction(
   formData: FormData
 ): Promise<FormState> {
   const usernameRaw = formData.get("username");
   const passwordRaw = formData.get("password");
+  const emailRaw = formData.get("email");
   const confirmPasswordRaw = formData.get("confirm-password");
 
   const schema = z.object({
     username: z.string(),
     password: z.string(),
+    email: z.string().nullish(),
     confirmPassword: z.string(),
   });
 
-  let invariantError: string | undefined;
+  const { data: payload } = schema.safeParse({
+    username: usernameRaw,
+    password: passwordRaw,
+    email: emailRaw,
+    confirmPassword: confirmPasswordRaw,
+  });
 
-  try {
-    const { username, password, confirmPassword } = schema.parse({
-      username: usernameRaw,
-      password: passwordRaw,
-      confirmPassword: confirmPasswordRaw,
-    });
+  if (!payload) return { error: "Invalid payload" };
+  if (payload.password !== payload.confirmPassword)
+    return { error: "Password Not match" };
 
-    if (password !== confirmPassword) {
-      invariantError = "Password confirmation does not match.";
-      throw new Error("confirm password does not match");
-    }
+  const { username, password, email } = payload;
+  const id = `u-${generateId(10)}`;
+  const newUserDbPayload: NewUserDbPayload = {
+    username,
+    id,
+    email,
+    hashedPassword: await hash(password),
+    parentId: id,
+  };
 
-    const newUser = await db.transaction(async (tx) => {
-      const existingUser = await tx
-        .select()
-        .from(user)
-        .where(eq(user.username, username));
+  const [data, invariantError] = await saveNewUser(newUserDbPayload);
+  if (invariantError || !data) return { error: invariantError };
 
-      if (existingUser.length > 0) {
-        invariantError = "Username not available";
-        tx.rollback();
-      }
-
-      const hashedPassword = await hash(password);
-
-      const id = `u-${generateId(10)}`;
-
-      const [newUser] = await tx
-        .insert(user)
-        .values({
-          id: id,
-          parentId: id,
-          username: username,
-          hashedPassword: hashedPassword,
-        })
-        .returning({ username: user.username, id: user.id });
-
-      return newUser;
-    });
-
-    return {
-      message: `Success creating new user: ${newUser.username} - ${newUser.id}`,
-    };
-  } catch (error) {
-    if (error instanceof Error) {
-      return {
-        error: invariantError ? invariantError : error.message,
-      };
-    }
-    return { error: "Internal Error" };
-  }
+  return {
+    message: `Success creating new user: ${data.username} - ${data.id}`,
+  };
 }
