3eb8a84fdfad903c02f179fb85df2d1ca18f9977
AR-Project <alvenresta@gmail.com>
2024-11-05 19:39:25 +0700

    fix/only display item that have been purchased

--- src/app/transaction/item/_loader/listOfItem.loader.ts
+++ src/app/transaction/item/_loader/listOfItem.loader.ts
@@ -1,11 +1,30 @@
 import db from "@/infrastructure/database/db";
 import { items } from "@/lib/schema/item";
+import { purchasedItems } from "@/lib/schema/schema";
 import { asc, eq } from "drizzle-orm";
 
-export async function listOfItemsLoader(userId: string) {
-  return await db
-    .select()
-    .from(items)
-    .orderBy(asc(items.name))
-    .where(eq(items.ownerId, userId));
+type Data = {
+  id: string;
+  name: string;
+};
+
+export async function listOfItemsLoader(requesterId: string): Promise<Data[]> {
+  const result = await db.query.purchasedItems.findMany({
+    where: eq(purchasedItems.ownerId, requesterId),
+    columns: {
+      itemId: true,
+    },
+    with: {
+      item: {
+        columns: {
+          name: true,
+        },
+      },
+    },
+  });
+
+  return result.map((row) => ({
+    id: row.itemId,
+    name: row.item.name,
+  }));
 }
