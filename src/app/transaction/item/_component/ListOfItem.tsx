ff1f6fdccbaa8e2c1d6ea3d7f08760b84e2b4fc7
AR-Project <alvenresta@gmail.com>
2024-11-05 20:18:50 +0700

    feat/init seach filter for list of item
    style/item display to grid

---
+++ src/app/transaction/item/_component/ListOfItem.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+
+import useList from "@/presentation/hooks/useList";
+
+type Item = {
+  id: string;
+  name: string;
+};
+
+type Props = {
+  items: Item[];
+};
+
+export default function ListOfItem({ items }: Props) {
+  const { list, filteredList, search } = useList("", items);
+  const [query, setQuery] = useState<string>("");
+
+  return (
+    <div className="flex flex-col">
+      <input
+        type="text"
+        className="text-white bg-gray-800 h-10 px-2 mt-2 mb-6 "
+        placeholder="🔍 Cari Barang..."
+        value={query}
+        onChange={(e) => {
+          setQuery(e.target.value);
+          search(e.target.value);
+        }}
+      />
+      <div className="grid grid-cols-2 gap-1">
+        {query.length == 0 && list.map((item) => <ItemLinkCard item={item} />)}
+        {query && filteredList.map((item) => <ItemLinkCard item={item} />)}
+      </div>
+    </div>
+  );
+}
+
+type ItemLinkCardProps = {
+  item: Item;
+};
+
+function ItemLinkCard({ item }: ItemLinkCardProps) {
+  return (
+    <Link
+      className="h-14 px-2 bg-gray-800 hover:bg-gray-700 border border-gray-700 flex flex-row gap-2 justify-start items-center"
+      key={item.id}
+      href={`./item/detail/${item.id}`}
+    >
+      <div className="h-10 aspect-square bg-gray-600"></div>
+      <div>{item.name}</div>
+    </Link>
+  );
+}
--- src/app/transaction/item/page.tsx
+++ src/app/transaction/item/page.tsx
@@ -1,28 +1,17 @@
-import Link from "next/link";
-
 import { auth } from "@/auth";
 import LoginRequiredWarning from "@/app/_component/auth/LoginRequiredWarning";
 import { listOfItemsLoader } from "./_loader/listOfItem.loader";
+import ListOfItem from "./_component/ListOfItem";
 
 export default async function Page() {
   const session = await auth();
-  if (!session) {
-    return <LoginRequiredWarning />;
-  }
+  if (!session) return <LoginRequiredWarning />;
 
   const listOfItems = await listOfItemsLoader(session.user.parentId);
 
   return (
     <div className="flex flex-col gap-1 max-w-md mx-auto">
-      {listOfItems.map((item) => (
-        <Link
-          className="h-10 px-3 bg-gray-700 hover:bg-gray-600 border border-gray-400 flex flex-row justify-start items-center"
-          key={item.id}
-          href={`./item/detail/${item.id}`}
-        >
-          {item.name}
-        </Link>
-      ))}
+      <ListOfItem items={listOfItems} />
     </div>
   );
 }
