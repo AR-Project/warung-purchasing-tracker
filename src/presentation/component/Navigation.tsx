9e5b86bb89efbca78a1217b02f33fa58cc09c7f6
AR-Project <alvenresta@gmail.com>
2024-11-07 16:50:17 +0700

    style/change layout from text only to icon center
    feat/add icon to nav

--- src/presentation/component/Navigation.tsx
+++ src/presentation/component/Navigation.tsx
@@ -1,31 +1,35 @@
 import AuthContainer from "@/app/_component/auth/AuthContainer";
 import { auth } from "@/auth";
 import Link from "next/link";
+import { MdHistory, MdReceiptLong } from "react-icons/md";
 
 export async function Navigation() {
   const session = await auth();
   const links = [
     {
       href: "/create",
-      label: "Create",
+      label: "New",
+      icon: <MdReceiptLong className="text-xl/3" />,
     },
     {
       href: "/transaction",
-      label: "Transaction",
+      label: "Activity",
+      icon: <MdHistory className="text-xl/3" />,
     },
   ];
 
   return (
-    <nav className="flex w-full max-w-md mx-auto flex-row gap-3 bg-transparent text-xs  justify-between items-center border-b border-white/20 px-1 h-14 bg-gray-500">
-      <div className="flex flex-row gap-2">
+    <nav className="flex w-full max-w-md mx-auto p-0.5 flex-row gap-3 bg-transparent text-xs  justify-between items-center border-b border-white/20 px-1 bg-gray-500">
+      <div className="flex flex-row gap-1">
         {session ? (
           links.map((link) => (
             <Link
-              className="uppercase bg-blue-800 p-2 hover:bg-blue-500"
+              className="bg-blue-800 w-20 hover:bg-blue-500 p-1 flex flex-col justify-center items-center"
               key={link.href}
               href={link.href}
             >
-              {link.label}
+              <div>{link.icon}</div>
+              <div className="text-[0.5rem] font-light">{link.label}</div>
             </Link>
           ))
         ) : (
