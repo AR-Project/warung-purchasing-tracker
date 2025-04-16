098b21e37ee2c3a0be90dfc07a1e160174a19b7f
AR-Project <alvenresta@gmail.com>
2025-04-16 22:07:07 +0700

    feat/initial implement createCategory Action

---
+++ src/app/manage/category/_action/createCategory.action.ts
@@ -0,0 +1,33 @@
+"use server";
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
+
+import { auth } from "@/auth";
+import { createCategory } from "@/infrastructure/repository/itemRepo";
+import { CreateCategoryDbPayload } from "@/lib/schema/item";
+import { generateId } from "@/lib/utils/generator";
+
+const createCtgryReqSchema = z.string();
+
+export default async function createCategoryAction(formData: FormData) {
+  const session = await auth();
+  if (session === null) return { error: "forbidden" };
+
+  const { data: categoryName, error: validationError } =
+    createCtgryReqSchema.safeParse(formData.get("category-name"));
+
+  if (validationError !== null) return { error: "invalid request" };
+
+  const payload: CreateCategoryDbPayload = {
+    id: `cat-${generateId(9)}`,
+    name: categoryName,
+    ownerId: session.user.userId,
+    creatorId: session.user.parentId,
+  };
+
+  const [createdCategoryId, dbError] = await createCategory(payload);
+  if (dbError !== null) return { error: dbError };
+
+  revalidatePath("/manage/category");
+  return { message: "Category Created", data: createdCategoryId };
+}
