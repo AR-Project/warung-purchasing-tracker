23fb24ebc54915dc3500d3448787e8bcb7ab0be0
AR-Project <alvenresta@gmail.com>
2025-04-21 19:08:42 +0700

    chore/edit error message for clarity

--- src/lib/utils/auth.ts
+++ src/lib/utils/auth.ts
@@ -10,24 +10,27 @@ export async function getUserInfo(): Promise<UserSession> {
 
 export async function validateUser(): Promise<SafeResult<UserSession>> {
   const session = await auth();
-  return session
-    ? [session.user, null]
-    : [null, "user not valid / user logged out"];
+  return session ? [session.user, null] : [null, "not_authenticated"];
 }
+
+type VerifyUserAccessError =
+  | "not_authenticated"
+  | "not_authorized"
+  | "internal_error";
 
 export async function verifyUserAccess(
   allowedRole: AvailableUserRole[]
-): Promise<SafeResult<UserSession>> {
+): Promise<SafeResult<UserSession, VerifyUserAccessError>> {
   const [user, validateUserError] = await validateUser();
-  if (validateUserError !== null) return [null, validateUserError];
+  if (validateUserError !== null) return [null, "not_authenticated"];
 
   // auth fron authjs only check from jwt. This line make sure active role
   // is verified directly from database
   const [currentUserRole, getUserRoleError] = await getUserRole(user.userId);
-  if (getUserRoleError !== null) return [null, getUserRoleError];
+  if (getUserRoleError !== null) return [null, "internal_error"];
 
   if (allowedRole.includes(currentUserRole) === false)
-    return [null, "user don't have apropiate role"];
+    return [null, "not_authorized"];
 
   return [user, null];
 }
