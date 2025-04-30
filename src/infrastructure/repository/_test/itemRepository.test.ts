236bfff22f2634df3cc16bf2e78d5e04ee2d29de
AR-Project <alvenresta@gmail.com>
2025-04-30 23:57:25 +0700

    feat/implement update, updatesortorder, delete category repo and its test

--- src/infrastructure/repository/_test/itemRepository.test.ts
+++ src/infrastructure/repository/_test/itemRepository.test.ts
@@ -5,7 +5,17 @@ import {
   defaultHelperUser,
 } from "./helper/userTableHelper";
 import { categoryTableHelper } from "./helper/itemTableHelper";
-import { createCategory } from "../itemRepo";
+import {
+  createCategory,
+  deleteCategory,
+  DeleteCategoryRepoPayload,
+  getCategoryByParentId,
+  UpdateCategoryDBPayload,
+  updateCategoryRepo,
+  updateCategorySortOrderDb,
+  updateCategorySortOrderRepo,
+} from "../itemRepo";
+import { aw } from "vitest/dist/chunks/reporters.C4ZHgdxQ.js";
 
 describe("itemRepository", () => {
   beforeAll(async () => {
@@ -33,7 +43,7 @@ describe("itemRepository", () => {
       expect(error).toBeNull();
       expect(persistedCategory.length).toBe(1);
     });
-    test("return error if category name is used", async () => {
+    test("return error if category name is used from same user", async () => {
       const preload: CreateCategoryDbPayload = {
         id: "cat-999",
         name: "duplicate",
@@ -89,6 +99,187 @@ describe("itemRepository", () => {
       expect(persisted01.sortOrder).toBe(1);
       expect(persisted02.sortOrder).toBe(2);
       expect(persisted03.sortOrder).toBe(3);
+    });
+  });
+
+  describe("updateCategory method", () => {
+    afterEach(async () => {
+      await categoryTableHelper.clean();
+    });
+
+    test("should fail if id is not exist ", async () => {
+      await categoryTableHelper.addMultiple(4);
+
+      const testPayload: UpdateCategoryDBPayload = {
+        id: "",
+        name: "",
+        requesterParentId: "",
+      };
+
+      const [result, error] = await updateCategoryRepo(testPayload);
+      const currentTotalCategoryRow = await categoryTableHelper.findAll();
+
+      expect(error).toBe("Invalid category ID");
+      expect(result).toBeNull();
+      expect(currentTotalCategoryRow.length).toBe(4);
+    });
+
+    test("should persist change to updated category", async () => {
+      await categoryTableHelper.addMultiple(5);
+      const testPayload: UpdateCategoryDBPayload = {
+        id: "cat-003",
+        name: "changed-category-name",
+        requesterParentId: defaultHelperUser.id,
+      };
+
+      const [result, error] = await updateCategoryRepo(testPayload);
+      const changedCategory = await categoryTableHelper.findById("cat-003");
+
+      expect(error).toBeNull();
+      expect(result).toBe("cat-003");
+      expect(changedCategory.length).toBe(1);
+      expect(changedCategory[0].name).toBe("changed-category-name");
+    });
+  });
+
+  describe("updateCategorySortOrder method", () => {
+    afterEach(async () => {
+      await categoryTableHelper.clean();
+    });
+
+    test("should fail when passed with empty payload", async () => {
+      await categoryTableHelper.addMultiple(3);
+      const payload: updateCategorySortOrderDb = {
+        newOrder: [],
+        requesterUserParentId: "",
+      };
+
+      const [result, error] = await updateCategorySortOrderRepo(payload);
+
+      expect(result).toBeNull();
+      expect(error).toBe("empty category");
+    });
+
+    test("should fail when passed with empty payload but correct user", async () => {
+      await categoryTableHelper.addMultiple(3);
+      const payload: updateCategorySortOrderDb = {
+        newOrder: [],
+        requesterUserParentId: defaultHelperUser.id,
+      };
+
+      const [result, error] = await updateCategorySortOrderRepo(payload);
+
+      expect(result).toBeNull();
+      expect(error).toBe("newOrder invalid");
+    });
+
+    test("should fail when passed with different length for newOrder", async () => {
+      await categoryTableHelper.addMultiple(3);
+      const payload: updateCategorySortOrderDb = {
+        newOrder: ["cat-002", "cat-001"],
+        requesterUserParentId: defaultHelperUser.id,
+      };
+
+      const [result, error] = await updateCategorySortOrderRepo(payload);
+
+      expect(result).toBeNull();
+      expect(error).toBe("newOrder invalid");
+    });
+
+    test("should success and persist data", async () => {
+      await categoryTableHelper.addMultiple(4);
+
+      const payload: updateCategorySortOrderDb = {
+        newOrder: ["cat-001", "cat-002", "cat-000", "cat-003"],
+        requesterUserParentId: defaultHelperUser.id,
+      };
+
+      const originalOrder = await getCategoryByParentId(defaultHelperUser.id);
+      const [result, error] = await updateCategorySortOrderRepo(payload);
+
+      const updatedOrder = await getCategoryByParentId(defaultHelperUser.id);
+
+      expect(error).toBeNull();
+      expect(result).toBe("ok");
+      expect(originalOrder.length).toBe(4);
+      expect(updatedOrder.length).toBe(4);
+      expect(originalOrder[0].sortOrder).toBe(0);
+      expect(originalOrder[1].sortOrder).toBe(1);
+      expect(originalOrder[2].sortOrder).toBe(2);
+      expect(originalOrder[3].sortOrder).toBe(3);
+      expect(originalOrder[0].id).toBe("cat-000");
+      expect(originalOrder[1].id).toBe("cat-001");
+      expect(originalOrder[2].id).toBe("cat-002");
+      expect(originalOrder[3].sortOrder).toBe(3);
+
+      expect(updatedOrder[0].sortOrder).toBe(0);
+      expect(updatedOrder[1].sortOrder).toBe(1);
+      expect(updatedOrder[2].sortOrder).toBe(2);
+      expect(updatedOrder[3].sortOrder).toBe(3);
+
+      expect(updatedOrder[0].id).toBe("cat-001");
+      expect(updatedOrder[1].id).toBe("cat-002");
+      expect(updatedOrder[2].id).toBe("cat-000");
+      expect(updatedOrder[3].id).toBe("cat-003");
+    });
+
+    //  create mock categories with correct user
+    // Should fail when method called with wrong credential - not an owner
+    // Should fail when method called with bad array/payload, such as:
+    //  different total category
+    //  note: array shape is already validated in SA. Method only need to worry about correct length and correct ids
+    // shoud success
+  });
+
+  describe("deleteCategory method", () => {
+    afterEach(async () => {
+      await categoryTableHelper.clean();
+    });
+
+    test("should fail when payload is invalid", async () => {
+      await categoryTableHelper.addMultiple(4);
+
+      const payload: DeleteCategoryRepoPayload = {
+        categoryId: "",
+        requesterParentId: "",
+      };
+
+      const [result, error] = await deleteCategory(payload);
+
+      expect(result).toBeNull();
+      expect(error).toBe("invalid payload");
+    });
+    test("should fail when payload is categoryId is invalid", async () => {
+      await categoryTableHelper.addMultiple(4);
+
+      const payload: DeleteCategoryRepoPayload = {
+        categoryId: "cat-999",
+        requesterParentId: defaultHelperUser.id,
+      };
+
+      const [result, error] = await deleteCategory(payload);
+
+      expect(result).toBeNull();
+      expect(error).toBe("invalid payload");
+    });
+    test("should success with valid payload", async () => {
+      await categoryTableHelper.addMultiple(4);
+
+      const payload: DeleteCategoryRepoPayload = {
+        categoryId: "cat-001",
+        requesterParentId: defaultHelperUser.id,
+      };
+
+      const categoriesPreDelete = await categoryTableHelper.findAll();
+      const [result, error] = await deleteCategory(payload);
+      const categoriesPostDelete = await categoryTableHelper.findAll();
+      const emptyCategory = await categoryTableHelper.findById("cat-001");
+
+      expect(error).toBeNull();
+      expect(result).toBe("ok");
+      expect(categoriesPreDelete.length).toBe(4);
+      expect(categoriesPostDelete.length).toBe(3);
+      expect(emptyCategory.length).toBe(0);
     });
   });
 });
--- src/infrastructure/repository/itemRepo.ts
+++ src/infrastructure/repository/itemRepo.ts
@@ -1,12 +1,21 @@
+import { and, asc, eq, inArray, sql, SQL } from "drizzle-orm";
+
+import { arraysHaveEqualElements } from "@/lib/utils/validator";
 import { category, CreateCategoryDbPayload } from "@/lib/schema/item";
 import db from "../database/db";
-import { eq } from "drizzle-orm";
 
 type CategoryId = string;
 type ErrorMessage = string;
 type DefaultResult<T = string> = [T, null] | [null, ErrorMessage];
 
 type CreateCategoryResult = DefaultResult<CategoryId>;
+
+export async function getCategoryByParentId(parentId: string) {
+  return await db.query.category.findMany({
+    where: eq(category.ownerId, parentId),
+    orderBy: [asc(category.sortOrder)],
+  });
+}
 
 export async function createCategory(
   payload: CreateCategoryDbPayload
@@ -37,5 +46,137 @@ export async function createCategory(
     return [categoryCreated.id, null];
   } catch (error) {
     return [null, invariantError ? invariantError : "internal error"];
+  }
+}
+
+/**
+ * UPDATE CATEGORY
+ */
+
+export type UpdateCategoryDBPayload = {
+  id: string;
+  name: string;
+  requesterParentId: string;
+};
+
+type UpdateCategoryResult = DefaultResult<CategoryId>;
+
+export async function updateCategoryRepo(
+  payload: UpdateCategoryDBPayload
+): Promise<UpdateCategoryResult> {
+  let invariantError: string | undefined;
+
+  try {
+    const updatedCategoryId = await db.transaction(async (tx) => {
+      const existedCategory = await tx
+        .select()
+        .from(category)
+        .where(eq(category.id, payload.id));
+
+      if (existedCategory.length === 0) {
+        invariantError = "Invalid category ID";
+        tx.rollback();
+      }
+
+      const [updatedCategory] = await tx
+        .update(category)
+        .set({ name: payload.name, modifiedAt: new Date() })
+        .where(eq(category.id, payload.id))
+        .returning({ id: category.id });
+
+      return updatedCategory.id;
+    });
+    return [updatedCategoryId, null];
+  } catch (error) {
+    return [null, invariantError ? invariantError : "internal error"];
+  }
+}
+
+/**
+ * UPDATE CATEGORY SORT ORDER
+ */
+
+export type updateCategorySortOrderDb = {
+  newOrder: Array<string>;
+  requesterUserParentId: string;
+};
+
+export async function updateCategorySortOrderRepo(
+  payload: updateCategorySortOrderDb
+): Promise<DefaultResult<"ok">> {
+  let invariantError: string | undefined;
+  try {
+    await db.transaction(async (tx) => {
+      const categories = await tx.query.category.findMany({
+        columns: {
+          id: true,
+        },
+        where: eq(category.ownerId, payload.requesterUserParentId),
+      });
+      if (categories.length === 0) {
+        invariantError = "empty category";
+        tx.rollback();
+      }
+      const mappedCategories = categories.map((ctg) => ctg.id);
+
+      if (
+        arraysHaveEqualElements(mappedCategories, payload.newOrder) === false
+      ) {
+        invariantError = "newOrder invalid";
+        tx.rollback();
+      }
+
+      // https://orm.drizzle.team/docs/guides/update-many-with-different-value
+      const sqlChunks: SQL[] = [];
+      sqlChunks.push(sql`(case`);
+      payload.newOrder.forEach((id, index) => {
+        sqlChunks.push(sql`when ${category.id} = ${id} then ${index}::INTEGER`);
+      });
+      sqlChunks.push(sql`end)`);
+      const finalSql: SQL = sql.join(sqlChunks, sql.raw(" "));
+
+      await tx
+        .update(category)
+        .set({ sortOrder: finalSql })
+        .where(inArray(category.id, payload.newOrder));
+    });
+    return ["ok", null];
+  } catch (error) {
+    return [null, invariantError ? invariantError : "Internal Error"];
+  }
+}
+
+/**
+ * DELETE CATEGORY
+ */
+
+export type DeleteCategoryRepoPayload = {
+  categoryId: string;
+  requesterParentId: string;
+};
+
+export async function deleteCategory(
+  payload: DeleteCategoryRepoPayload
+): Promise<DefaultResult<"ok">> {
+  let invariantError: string | undefined;
+  try {
+    await db.transaction(async (tx) => {
+      const categoryToDelete = await tx.query.category.findMany({
+        where: and(
+          eq(category.id, payload.categoryId),
+          eq(category.ownerId, payload.requesterParentId)
+        ),
+      });
+      if (categoryToDelete.length === 0) {
+        invariantError = "invalid payload";
+        tx.rollback();
+      }
+
+      // FUTURE: items with delete category set to null
+      await tx.delete(category).where(eq(category.id, payload.categoryId));
+    });
+    return ["ok", null];
+  } catch (error) {
+    return [null, invariantError ? invariantError : "Internal Error"];
   }
 }
