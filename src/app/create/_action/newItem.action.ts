da7ff175edcc8b01f99a08f0924e00d0d9a24140
AR-Project <alvenresta@gmail.com>
2025-05-08 18:21:42 +0700

    feat/adding default category when new item action via purchase creator

--- src/app/create/_action/newItem.action.ts
+++ src/app/create/_action/newItem.action.ts
@@ -7,7 +7,8 @@ import { z } from "zod";
 import db from "@/infrastructure/database/db";
 import { generateId } from "@/lib/utils/generator";
 import { getUserInfo } from "@/lib/utils/auth";
-import { items, NewItemDbPayload } from "@/lib/schema/item";
+import { item, NewItemDbPayload } from "@/lib/schema/item";
+import { user } from "@/lib/schema/user";
 
 export async function newItemAction(
   formData: FormData
@@ -22,17 +23,32 @@ export async function newItemAction(
       invariantError = "Invalid Payload";
       throw new Error(invariantError);
     }
+
+    // read user default category
+    const userDefaultCategory = await db.query.user.findFirst({
+      columns: {
+        defaultCategory: true,
+      },
+      where: eq(user.id, userId),
+    });
+
+    if (!userDefaultCategory || !userDefaultCategory.defaultCategory) {
+      invariantError = "intenal error - default category null";
+      throw new Error(invariantError);
+    }
+
     const newItemDbPayload: NewItemDbPayload = {
       id: generateId(10),
       name,
       ownerId: parentId,
       creatorId: userId,
+      categoryId: userDefaultCategory.defaultCategory,
     };
 
     const [savedItem] = await db
-      .insert(items)
+      .insert(item)
       .values(newItemDbPayload)
-      .returning({ id: items.id, name: items.name });
+      .returning({ id: item.id, name: item.name });
 
     revalidateTag("items");
     revalidatePath("/create");
