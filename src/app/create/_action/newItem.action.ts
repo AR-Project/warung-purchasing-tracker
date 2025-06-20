a011015bca156181125bed405ae75cdc442e7f11
AR-Project <alvenresta@gmail.com>
2025-06-20 19:01:00 +0700

    fix/utilize itemRepo  instead direct db call

--- src/app/create/_action/newItem.action.ts
+++ src/app/create/_action/newItem.action.ts
@@ -1,67 +1,37 @@
 "use server";
 
-import { DrizzleError, eq } from "drizzle-orm";
 import { revalidatePath, revalidateTag } from "next/cache";
 import { z } from "zod";
 
-import db from "@/infrastructure/database/db";
+import itemRepo, {
+  CreateItemRepoPayload,
+} from "@/infrastructure/repository/itemRepo";
 import { generateId } from "@/lib/utils/generator";
-import { getUserInfo } from "@/lib/utils/auth";
-import { item, NewItemDbPayload } from "@/lib/schema/item";
-import { user } from "@/lib/schema/user";
+import { verifyUserAccess } from "@/lib/utils/auth";
 
 export async function newItemAction(
   formData: FormData
 ): Promise<FormState<Item>> {
+  const allowedRole: AvailableUserRole[] = ["staff", "admin", "manager"];
+  const [user, authError] = await verifyUserAccess(allowedRole);
+  if (authError) return { error: authError };
+
   const nameRaw = formData.get("name");
-  let invariantError: string | undefined;
+  const { data: name, error: payloadError } = z.string().safeParse(nameRaw);
+  if (payloadError) return { error: "invalid payload" };
 
-  try {
-    const { userId, parentId } = await getUserInfo();
-    const { data: name } = z.string().safeParse(nameRaw);
-    if (!name) {
-      invariantError = "Invalid Payload";
-      throw new Error(invariantError);
-    }
+  const repoPayload: CreateItemRepoPayload = {
+    id: generateId(10),
+    name: name,
+    ownerId: user.parentId,
+    creatorId: user.userId,
+  };
 
-    // read user default category
-    const userDefaultCategory = await db.query.user.findFirst({
-      columns: {
-        defaultCategory: true,
-      },
-      where: eq(user.id, userId),
-    });
+  const [addedItem, repoError] = await itemRepo.create(repoPayload);
+  if (repoError || !addedItem) return { error: repoError };
 
-    if (!userDefaultCategory || !userDefaultCategory.defaultCategory) {
-      invariantError = "intenal error - default category null";
-      throw new Error(invariantError);
-    }
+  revalidateTag("items");
+  revalidatePath("/create");
 
-    const newItemDbPayload: NewItemDbPayload = {
-      id: generateId(10),
-      name,
-      ownerId: parentId,
-      creatorId: userId,
-      categoryId: userDefaultCategory.defaultCategory,
-    };
-
-    const [savedItem] = await db
-      .insert(item)
-      .values(newItemDbPayload)
-      .returning({ id: item.id, name: item.name });
-
-    revalidateTag("items");
-    revalidatePath("/create");
-    return {
-      message: `Item ${nameRaw} created`,
-      data: savedItem,
-    };
-  } catch (error) {
-    if (error instanceof Error && error.message == "USER_LOGGED_OUT") {
-      return { error: "Login first" };
-    }
-    if (invariantError) return { error: invariantError };
-    if (error instanceof DrizzleError) return { error: error.message };
-    return { error: "internal error" };
-  }
+  return { message: "success", data: addedItem };
 }
