05a81b9cf88e9e18ce34ba010f4a19bd4b77ec18
AR-Project <alvenresta@gmail.com>
2025-06-24 17:05:10 +0700

    fix/improve detection in case of duplicate id that will undected by set

--- src/lib/utils/validator.ts
+++ src/lib/utils/validator.ts
@@ -92,10 +92,12 @@ export function parseSearchParams(params: SearchParams): SearchFilter {
 
 export function arraysHaveEqualElements(array1: string[], array2: string[]) {
   if (array1.length !== array2.length) return false;
-  const set = new Set(array2);
+  const set1 = new Set(array1);
+  const set2 = new Set(array2);
+  if (set1.size !== set2.size) return false;
 
   for (const element of array1) {
-    if (!set.has(element)) return false;
+    if (!set2.has(element)) return false;
   }
   return true;
 }
