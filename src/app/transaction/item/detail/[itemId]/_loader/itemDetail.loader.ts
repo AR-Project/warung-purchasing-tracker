5fcdcdd1af7c9980c3a66d1d4c0c5a54c2d36c34
AR-Project <alvenresta@gmail.com>
2025-06-28 16:02:43 +0700

    feat/add category name on loader result

--- src/app/transaction/item/detail/[itemId]/_loader/itemDetail.loader.ts
+++ src/app/transaction/item/detail/[itemId]/_loader/itemDetail.loader.ts
@@ -5,21 +5,6 @@ import { DateTime } from "luxon";
 import { item } from "@/lib/schema/item";
 import { purchasedItem } from "@/lib/schema/purchase";
 
-type LoaderResponse = {
-  purchaseItemHistory: {
-    purchaseId: string;
-    purchaseDate: Date;
-    quantityInHundreds: number;
-    pricePerUnit: number;
-    totalPrice: number;
-  }[];
-  id: string;
-  name: string;
-  imageId: string | null;
-  createdAt: Date;
-  modifiedAt: Date;
-};
-
 export async function itemDetailLoader(requestedItemId: string) {
   const result = await db.query.item.findFirst({
     where: eq(item.id, requestedItemId),
@@ -42,6 +27,11 @@ export async function itemDetailLoader(requestedItemId: string) {
       creator: {
         columns: {
           username: true,
+        },
+      },
+      category: {
+        columns: {
+          name: true,
         },
       },
     },
