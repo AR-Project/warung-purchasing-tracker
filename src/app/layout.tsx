b8892f8821ba57dca951c3931f1849438ae3a37b
AR-Project <alvenresta@gmail.com>
2025-07-20 00:46:00 +0700

    feat/adding flex to body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-black text-white`}>
+      <body className={`${inter.className} flex flex-col bg-black text-white`}>
         <Navigation userData={userSessionWithRole} />
         {children}
         <ToastContainer
