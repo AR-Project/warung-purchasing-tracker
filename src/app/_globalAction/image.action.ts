188b69e2f654aa8612314aa44d289d2b00876c8f
AR-Project <alvenresta@gmail.com>
2025-08-04 17:42:12 +0700

    refactor/upgrade old code following new standard

--- src/app/_globalAction/image.action.ts
+++ src/app/_globalAction/image.action.ts
@@ -2,38 +2,50 @@
 
 import { revalidatePath } from "next/cache";
 import { eq } from "drizzle-orm";
-import path from "path";
 import { promises as fs } from "fs";
 import { z } from "zod";
+import { DateTime } from "luxon";
 
-import { auth } from "@/auth";
 import db from "@/infrastructure/database/db";
 import { writeImageFile } from "@/infrastructure/storage/localStorage";
+
 import { image, NewImageDbPayload } from "@/lib/schema/image";
+import { verifyUserAccess } from "@/lib/utils/auth";
+import { generateImageFilePath } from "@/lib/utils/fileSystem";
+
+const imageSchema = z.custom<Blob>(
+  (data) => data instanceof Blob && data.type === "image/jpeg"
+);
 
 export async function uploadImage(
-  prevState: any,
   formData: FormData
 ): Promise<FormState<string>> {
-  const session = await auth();
-  if (!session) return { error: "Must login first" };
+  const [user, authError] = await verifyUserAccess([
+    "admin",
+    "manager",
+    "staff",
+  ]);
+  if (authError) return { error: authError };
 
-  const payload = formData.get("image");
-  const imageSchema = z.custom<Blob>(
-    (data) => data instanceof Blob && data.type === "image/jpeg"
-  );
+  const imageBlobRaw = formData.get("image");
+  const currentPathRaw = formData.get("current-path");
 
-  const { data: img } = imageSchema.safeParse(payload);
-  if (!img) return { error: "Expect a image" };
+  const { data: currentPath, error: currentPathError } = z
+    .string()
+    .safeParse(currentPathRaw);
+  if (currentPathError) return { error: "current path is not defined" };
 
-  const [metadata] = await writeImageFile(img);
-  if (!metadata) return { error: "Fail to save the image" };
+  const { data: imageBlob } = imageSchema.safeParse(imageBlobRaw);
+  if (!imageBlob) return { error: "Expect a image" };
+
+  const [metadata, writeError] = await writeImageFile(imageBlob);
+  if (writeError !== null) return { error: "Fail to save the image" };
 
   const newImageDbPayload: NewImageDbPayload = {
     id: metadata.id,
     path: metadata.path,
-    ownerId: session.user.parentId,
-    creatorId: session.user.userId,
+    ownerId: user.parentId,
+    creatorId: user.userId,
     originalFileName: "blob",
   };
 
@@ -42,7 +54,7 @@ export async function uploadImage(
       .insert(image)
       .values(newImageDbPayload)
       .returning({ id: image.id });
-    revalidatePath("/library");
+    revalidatePath(currentPath);
     return { message: "Image uploaded successfully", data: savedImage.id };
   } catch (error) {
     console.error("Upload error:", error);
@@ -50,36 +62,57 @@ export async function uploadImage(
   }
 }
 
+const removeImageSchema = z.object({
+  imageId: z.string(),
+  currentPath: z.string(),
+});
+
 export async function removeImage(
-  prevState: any,
   formData: FormData
 ): Promise<FormState<string>> {
-  const session = await auth();
-  if (!session) return { error: "Must login first" };
-  const { user } = session;
+  const [user, authError] = await verifyUserAccess([
+    "admin",
+    "guest",
+    "manager",
+  ]);
+  if (authError) return { error: authError };
 
-  const payload = formData.get("id");
-  if (typeof payload !== "string") return { error: "No payload" };
+  const { data: payload, error: payloadError } = removeImageSchema.safeParse({
+    imageId: formData.get("id"),
+    currentPath: formData.get("current-path"),
+  });
+  if (payloadError) return { error: "Invalid Payload" };
 
   try {
     const [resultImage] = await db
       .select()
       .from(image)
-      .where(eq(image.id, payload));
+      .where(eq(image.id, payload.imageId));
     if (!resultImage) throw new Error("Image Not Found");
 
     if (![resultImage.creatorId, resultImage.ownerId].includes(user.userId))
       throw new Error("Unauthorized");
 
-    const imageFilePath = path.join(process.cwd(), resultImage.path);
+    const uploadAtDateString = DateTime.fromJSDate(
+      resultImage.uploadedAt
+    ).toISODate();
+    if (!uploadAtDateString) throw new Error("Date Invalid");
+
+    const imageFilePath = generateImageFilePath(
+      payload.imageId,
+      uploadAtDateString
+    );
 
     await fs.stat(imageFilePath);
     await fs.unlink(imageFilePath);
-    await db.delete(image).where(eq(image.id, payload));
-    revalidatePath("/library");
+
+    await db.delete(image).where(eq(image.id, payload.imageId));
+    revalidatePath(payload.currentPath);
+
     return { message: "Image deleted successfully" };
   } catch (error) {
     console.error("Delete error:", error);
+    console.log("Payload : " + JSON.stringify(payload, null, 4));
     return { error: "Failed to delete image" };
   }
 }
