c7817d5275736d9651b2204d545d3f14da6d17b0
AR-Project <alvenresta@gmail.com>
2025-08-07 16:21:13 +0700

    chore/refactor code implement safe promise and flaten if statement.

--- src/app/_globalAction/loginUser.action.ts
+++ src/app/_globalAction/loginUser.action.ts
@@ -1,24 +1,28 @@
 "use server";
 
 import { AuthError } from "next-auth";
+
 import { signIn } from "@/auth";
+import { safePromise } from "@/lib/utils/safePromise";
 
 export async function loginUserAction(formData: FormData) {
-  try {
-    await signIn("credentials", formData);
-    return {};
-  } catch (error) {
-    if (error instanceof AuthError) {
-      return { error: error.cause?.err?.message };
-    }
+  const { error: signInRes } = await safePromise(
+    signIn("credentials", formData)
+  );
 
-    if (error instanceof Error) {
-      if (error.message === "NEXT_REDIRECT") {
-        throw error;
-      }
-      return { error: error.message };
-    }
+  const isAuthError = signInRes instanceof AuthError;
+  const isAuthSuccess =
+    signInRes instanceof Error && signInRes.message === "NEXT_REDIRECT";
 
+  // SignIn Failure due client error
+  if (isAuthError) return { error: "Invalid credentials" };
+
+  if (isAuthSuccess) {
+    // SignIn Success - throw error to trigger next redirect
+    throw signInRes;
+  } else {
+    // SignIn Failure due server error
+    console.error(signInRes);
     return { error: "internal error" };
   }
 }
