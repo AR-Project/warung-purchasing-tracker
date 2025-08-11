0722690fce79a639bd018fff6377b050374b5d9b
AR-Project <alvenresta@gmail.com>
2025-08-11 19:12:07 +0700

    layout/move PurchaseDateEditor up
    chore/clean up

--- src/app/transaction/details/[purchaseId]/edit/page.tsx
+++ src/app/transaction/details/[purchaseId]/edit/page.tsx
@@ -2,35 +2,33 @@ import Link from "next/link";
 import { notFound } from "next/navigation";
 import { CgReorder } from "react-icons/cg";
 
-import { auth } from "@/auth";
+import { validateUser } from "@/lib/utils/auth";
 import LoginRequiredWarning from "@/app/_component/auth/LoginRequiredWarning";
 import { BackButton } from "@/app/_component/BackButton";
 
 import getUserItems from "@/app/_loader/getUserItems.loader";
 import getUserVendors from "@/app/_loader/getUserVendors.loader";
+
 import { singlePurchaseLoader } from "../_loader/singlePurchase.loader";
-import { PurchaseDataEditor } from "./_component/PurchaseDataEditor";
 import PurchaseItemEditor from "./_component/PurchaseItemEditor";
 import VendorEditor from "./_component/VendorEditor";
 import PurchaseDeleteDialog from "./_component/PurchaseDeleteDialog";
 import PurchaseItemUpdater from "./_component/PurchaseItemUpdater";
+import PurchaseDateEditor from "./_component/PurchaseDateEditor";
 
 type Props = {
   params: Promise<{ purchaseId: string }>;
 };
 
 export default async function Page({ params }: Props) {
-  const session = await auth();
-  if (!session) {
+  const [user] = await validateUser();
+  if (!user) {
     return <LoginRequiredWarning />;
   }
 
   const { purchaseId: purchaseIdParams } = await params;
-
   const details = await singlePurchaseLoader(purchaseIdParams);
-  if (!details) {
-    notFound();
-  }
+  if (!details) return notFound();
 
   const {
     items: purchaseItems,
@@ -39,8 +37,8 @@ export default async function Page({ params }: Props) {
     vendorId,
     vendorName,
   } = details;
-  const initialItems = await getUserItems(session.user.parentId);
-  const initialVendors = await getUserVendors(session.user.parentId);
+  const initialItems = await getUserItems(user.parentId);
+  const initialVendors = await getUserVendors(user.parentId);
 
   return (
     <div className="flex flex-col w-full max-w-md m-auto gap-2">
@@ -51,13 +49,20 @@ export default async function Page({ params }: Props) {
         </div>
         <PurchaseDeleteDialog purchaseId={purchaseId} />
       </div>
-      <PurchaseDataEditor purchase={details} />
+
+      <PurchaseDateEditor
+        currentDate={details.purchasesAt}
+        purchaseId={details.id}
+      />
       <VendorEditor
         currentVendor={{ id: vendorId, name: vendorName }}
         initialVendor={initialVendors}
         purchaseId={purchaseId}
       />
-      <div className="_SEPARATOR w-full border-b border-white/20"></div>
+      <div
+        data-comment="SEPARATOR"
+        className="w-full border-b border-white/20"
+      ></div>
 
       <div className="flex flex-row gap-2 m-auto justify-end w-full">
         <Link
