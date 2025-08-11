a831fdf1bbe04e57e81f394dd1b141ee1740c772
AR-Project <alvenresta@gmail.com>
2025-08-11 22:17:55 +0700

    chore/refactor and optimize code

--- src/app/manage/staff/_action/updateUserRole.action.ts
+++ src/app/manage/staff/_action/updateUserRole.action.ts
@@ -7,61 +7,37 @@ import { revalidatePath } from "next/cache";
 import { auth } from "@/auth";
 import db from "@/infrastructure/database/db";
 import { user } from "@/lib/schema/user";
-import { adminOnlyRole } from "@/lib/const";
+import { adminManagerRole, adminOnlyRole } from "@/lib/const";
+import { verifyUserAccess } from "@/lib/utils/auth";
+import ClientError from "@/lib/exception/ClientError";
+import { actionErrorDecoder } from "@/lib/exception/errorDecoder";
+
+const schema = z.object({
+  childUserId: z.string().min(1),
+  newRole: z
+    .string()
+    .refine((val) => ["manager", "staff", "guest"].includes(val)),
+});
 
 export async function updateUserRole(formdata: FormData): Promise<FormState> {
-  const session = await auth();
-  if (!session) return { error: "Forbidden" };
-
-  const childUserIdRaw = formdata.get("child-user-id");
-  const newRoleRaw = formdata.get("new-role");
-
-  const availableRole = ["manager", "staff", "guest"];
-
-  const schema = z.object({
-    childUserId: z.string().min(1),
-    newRole: z.string().refine((val) => availableRole.includes(val)),
-  });
+  const [userInfo, authError] = await verifyUserAccess(adminManagerRole);
+  if (authError) return { error: authError };
 
   const { data: payload } = schema.safeParse({
-    childUserId: childUserIdRaw,
-    newRole: newRoleRaw,
+    childUserId: formdata.get("child-user-id"),
+    newRole: formdata.get("new-role"),
   });
-  if (!payload) {
-    return { error: "Invalid Payload" };
-  }
+  if (!payload) return { error: "Invalid Payload" };
 
-  let invariantError: string | undefined;
   try {
     await db.transaction(async (tx) => {
-      // Validate requester user
-      const requesterUsers = await tx
-        .select({ role: user.role })
-        .from(user)
-        .where(eq(user.id, session.user.userId));
-      if (requesterUsers.length === 0) {
-        invariantError = "Invalid requesterId";
-        tx.rollback();
-      }
+      const userToUpdate = await tx.query.user.findFirst({
+        where: (user, { eq }) => eq(user.id, payload.childUserId),
+      });
+      if (!userToUpdate) throw new ClientError("invalid user");
+      if (userToUpdate.parentId !== userInfo.parentId)
+        throw new ClientError("Not allowed");
 
-      // Validate requester user role
-      const [requesterUser] = requesterUsers;
-      if (!adminOnlyRole.includes(requesterUser.role)) {
-        invariantError = "Your role is not allowed";
-        tx.rollback();
-      }
-
-      // Validate if user to modify exist
-      const usersToDelete = await tx
-        .select()
-        .from(user)
-        .where(eq(user.id, payload.childUserId));
-      if (usersToDelete.length === 0) {
-        invariantError = "User not exist";
-        tx.rollback();
-      }
-
-      // Append change user
       await tx
         .update(user)
         .set({
@@ -76,7 +52,6 @@ export async function updateUserRole(formdata: FormData): Promise<FormState> {
       message: `Role changed`,
     };
   } catch (error) {
-    if (error instanceof Error) console.log(error);
-    return { error: invariantError ? invariantError : "Internal Error" };
+    return actionErrorDecoder(error);
   }
 }
