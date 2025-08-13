a8173f45ccae0d83c98af67da051c05ab56aa409
AR-Project <alvenresta@gmail.com>
2025-08-13 18:51:12 +0700

    feat/adjust to new image file structure

--- src/app/manage/media/page.tsx
+++ src/app/manage/media/page.tsx
@@ -23,7 +23,7 @@ export default async function UploadImage() {
       <div className="grid grid-cols-3 gap-4">
         {imagesList.map((image) => (
           <div key={image.id} className="aspect-square relative">
-            <Image key={image.id} src={`/api/image/${image.id}`} fill alt="" />
+            <Image key={image.id} src={`/api/image/${image.url}`} fill alt="" />
             <RemoveImage id={image.id} />
           </div>
         ))}
