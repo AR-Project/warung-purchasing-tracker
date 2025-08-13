e25e35abc603c6d7dac6d4726b8bb8bae2a4549a
AR-Project <alvenresta@gmail.com>
2025-08-13 15:26:04 +0700

    chore/refactor

--- src/auth.ts
+++ src/auth.ts
@@ -1,13 +1,13 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { type JWT } from "next-auth/jwt";
-import { eq } from "drizzle-orm";
 import { z } from "zod";
 import { verify } from "@node-rs/argon2";
 import { revalidatePath } from "next/cache";
 
 import db from "./infrastructure/database/db";
-import { user } from "./lib/schema/user";
+import { logger } from "./lib/logger";
+import ClientError from "./lib/exception/ClientError";
 
 declare module "next-auth/jwt" {
   interface JWT extends UserSession {}
@@ -20,9 +20,6 @@ declare module "next-auth" {
     user: UserSession;
   }
 }
-
-const DEFAULT_ERROR_MSG = "Check again your username and/or password";
-const isEnvDev = process.env.NODE_ENV === "development";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -44,56 +41,44 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           .required();
 
         try {
-          const { data } = credentialsSchema.safeParse(credentials);
-          if (!data) {
-            throw new Error(
-              isEnvDev ? "payload schema error" : DEFAULT_ERROR_MSG
-            );
-          }
+          const { data, error: payloadError } =
+            credentialsSchema.safeParse(credentials);
+          if (payloadError) throw new ClientError("payload schema error");
 
           const { username, password } = data;
+          const user = await db.query.user.findFirst({
+            columns: {
+              username: true,
+              hashedPassword: true,
+              id: true,
+              parentId: true,
+            },
+            where: (user, { eq }) => eq(user.username, username),
+          });
+          if (!user) throw new ClientError("wrong username");
 
-          const users = await db
-            .select({
-              username: user.username,
-              hashedPassword: user.hashedPassword,
-              userId: user.id,
-              parentId: user.parentId,
-            })
-            .from(user)
-            .where(eq(user.username, username));
+          const isPasswordValid = await verify(user.hashedPassword, password);
+          if (!isPasswordValid)
+            throw new ClientError("username OK, password error");
 
-          if (users.length != 1) {
-            throw new Error(isEnvDev ? "wrong username" : DEFAULT_ERROR_MSG);
-          }
-
-          const [validUser] = users;
-
-          const isPasswordValid = await verify(
-            users[0].hashedPassword,
-            password
-          );
-
-          if (!isPasswordValid) {
-            throw new Error(
-              isEnvDev ? "username OK, password error" : DEFAULT_ERROR_MSG
-            );
-          }
           revalidatePath("/", "layout");
           return {
-            username: validUser.username,
-            userId: validUser.userId,
-            parentId: validUser.parentId,
+            username: user.username,
+            userId: user.id,
+            parentId: user.parentId,
           };
         } catch (error) {
-          if (error instanceof Error) {
-            throw new Error(error.message);
+          if (error instanceof Error && error.name !== "ClientError")
+            logger.error("INTERNAL_ERROR_AUTH");
+          if (error instanceof ClientError) {
+            throw error;
           }
-          throw new Error(isEnvDev ? "autorize catch block" : "Internal Error");
+          return null;
         }
       },
     }),
   ],
+  debug: true,
   callbacks: {
     jwt({ token, user }) {
       if (user) {
@@ -110,14 +95,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       return session;
     },
   },
-  trustHost: true,
+
   logger: {
-    error(code, ...message) {},
-    warn(code, ...message) {
-      console.log(code);
-    },
-    debug(code, ...message) {
-      console.log(code);
-    },
+    error(error) {},
+    warn(error) {},
+    debug(error) {},
   },
 });
