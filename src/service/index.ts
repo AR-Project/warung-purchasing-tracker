54641ed47b0a5aecc024c14525bbc8479cd153c9
AR-Project <alvenresta@gmail.com>
2025-08-20 16:04:21 +0700

    chore/refactor service

--- src/service/index.ts
+++
@@ -1,5 +0,0 @@
-import db from "@/infrastructure/database/db";
-import { createCategoryAndMoveItemFactory } from "./createCategoryAndMoveItem";
-
-export const createCategoryAndMoveItemService =
-  createCategoryAndMoveItemFactory(db);
